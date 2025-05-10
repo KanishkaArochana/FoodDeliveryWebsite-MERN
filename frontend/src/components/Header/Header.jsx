025babb592b7955ade859328a8da88962b33314b
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-05-10 17:09:53 +0530

    Create header in frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam harum error quo repellendus explicabo voluptate quia modi repudiandae vitae nam et facere debitis, architecto, maxime officiis quos similique neque sint vero dolores eum dicta. Est enim similique, eos deserunt omnis impedit quasi voluptatum autem! Debitis accusamus sequi excepturi facere dicta.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
+
+
