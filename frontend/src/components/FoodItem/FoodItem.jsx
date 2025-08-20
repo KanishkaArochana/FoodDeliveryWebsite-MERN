fabc4b38eecb06c610c8c93fcc9473e596c30358
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-08-20 18:55:53 +0530

    Update food item card

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,27 +1,47 @@
-import React, { useContext} from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
 
-const FoodItem = ({id,name, price,description,image}) => {
-
- 
-  const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart, url, token } = useContext(StoreContext);
 
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
-        <img className="food-item-image" src={url+"/images/"+image} alt=""  />
-        {!cartItems[id]
-            ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=''/>
-            : <div className="food-item-counter">
-              <img className='red' onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-              <p>{cartItems[id]}</p>
-              <img className='green' onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-            </div>
+        <img className="food-item-image" src={url + "/images/" + image} alt="" />
 
-        }
+        {/* Show Add/Remove buttons only if logged in (token exists) */}
+        {token ? (
+          !cartItems[id] ? (
+            <img 
+              className='add' 
+              onClick={() => addToCart(id)} 
+              src={assets.add_icon_white} 
+              alt='' 
+            />
+          ) : (
+            <div className="food-item-counter">
+              <img 
+                className='red' 
+                onClick={() => removeFromCart(id)} 
+                src={assets.remove_icon_red} 
+                alt="" 
+              />
+              <p>{cartItems[id]}</p>
+              <img 
+                className='green' 
+                onClick={() => addToCart(id)} 
+                src={assets.add_icon_green} 
+                alt="" 
+              />
+            </div>
+          )
+        ) : (
+          <p className="login-msg">Login to add items</p>
+        )}
       </div>
+
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p>
